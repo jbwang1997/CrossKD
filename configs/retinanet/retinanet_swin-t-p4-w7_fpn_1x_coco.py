9f3a01297006a4391525189904cf6181331e7ad6
cym <chenyuming@mail.nankai.edu.cn>
2023-07-22 02:01:47 +0800

    [Update] Add retinanet swinT to r50

---
+++ configs/retinanet/retinanet_swin-t-p4-w7_fpn_1x_coco.py
@@ -0,0 +1,32 @@
+_base_ = './retinanet_r50_fpn_1x_coco.py'
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='SwinTransformer',
+        embed_dims=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(1, 2, 3),
+        with_cp=False,
+        convert_weights=True,
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint=
+            'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth'
+        )),
+    neck=dict(
+        type='FPN',
+        in_channels=[192, 384, 768],
+        out_channels=256,
+        start_level=0,
+        add_extra_convs='on_input',
+        num_outs=5)
+    )
